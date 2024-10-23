782967eb552ec399bfa34d674c36601bed952f71
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-10-23 16:53:42 +0200

    Fix to double smash jump.

--- logic/player_smashing_state.gd
+++ logic/player_smashing_state.gd
@@ -8,10 +8,13 @@ class_name SmashingState
 func enter(ownr) -> void:
 	ownr.animation.play("smash")
 	ownr.velocity.y = ownr.max_fall_speed * 0.5
+	ownr.coyote_timer = 0.0
 
 func update(ownr: Knight, delta: float) -> void:
 	if ownr.is_on_floor():
+		ownr.queued_jump_timer = 0.0
 		ownr.change_state(ownr.idle_state)
+		return
 	else:
 		ownr.velocity.y += ownr.gravity * gravity_coefficient * delta
 		ownr.velocity.x = ownr.movement * controls_coefficient * ownr.speed
@@ -24,6 +27,7 @@ func handle_input(ownr: Knight) -> void:
 
 	if Input.is_action_just_pressed("jump") and ownr.coyote_timer > 0.0:
 		ownr.change_state(ownr.jumping_state)
+		return
 	
 func exit(ownr) -> void:
 	ownr.velocity.y = 0
