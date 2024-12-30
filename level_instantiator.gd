8effa3de961021d6cd35c3d4852796d3a053b5e9
Potato <potato@potatogam.es>
2024-12-30 19:57:11 +0100

    Fixes to the level instantiator. Threaded loading in instantiator.

--- level_instantiator.gd
+++ level_instantiator.gd
@@ -1,14 +1,29 @@
 extends Area2D
 
-@export var level_scene: String
-
 var level_placeholder: InstancePlaceholder
+var level_path: String
+
+var level_instance: Node
 
 func _ready():
-	level_placeholder = get_parent().get_node(level_scene)
+	set_process(false)
+	level_placeholder = get_parent()
+	level_path = level_placeholder.get_instance_path()
 
 	connect("body_entered", _on_body_entered)
 
+
+func _process(_delta):
+	var status = ResourceLoader.load_threaded_get_status(level_path)
+	if status != ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
+		return
+	set_process(false)
+	level_instance = level_placeholder.create_instance()
+
 func _on_body_entered(body):
+	if level_instance:
+		return
+
 	if body is Knight:
-		level_placeholder.create_instance()
+		ResourceLoader.load_threaded_request(level_path)
+		set_process(true)
